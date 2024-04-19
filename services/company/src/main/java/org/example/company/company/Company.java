6b88ecc46869a2c9371bc422954b4b7206b4f57b
Mark Bedoya <mrkbedoya@gmail.com>
2024-04-18 22:09:55 -0500

    remove unused imports

--- services/company/src/main/java/org/example/company/company/Company.java
+++ services/company/src/main/java/org/example/company/company/Company.java
@@ -1,12 +1,9 @@
 package org.example.company.company;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
