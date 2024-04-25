8edf24ed753e1ee244739765be283b53baff5b17
Mark Bedoya <mrkbedoya@gmail.com>
2024-04-25 01:22:46 -0500

    adding config-server default test case

--- services/config-server/src/test/java/org/example/configserver/ConfigServerApplicationTests.java
+++ services/config-server/src/test/java/org/example/configserver/ConfigServerApplicationTests.java
@@ -1,5 +1,7 @@
 package org.example.configserver;
 
+import static org.springframework.test.util.AssertionErrors.assertTrue;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +10,7 @@ class ConfigServerApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertTrue("defaulting to true.",true);
 	}
 
 }
